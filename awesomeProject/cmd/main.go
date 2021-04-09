b08fc752ce2ade9902fcb00015d57f0ffdafa413
tolik <tolikan200014@mail.ru>
2021-04-09 11:42:29 +0700

    testik2

--- awesomeProject/cmd/main.go
+++ awesomeProject/cmd/main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"strconv"
 )
 
 func main() {
-	var nowhere *int
-	var i, z, err int
+	http.HandleFunc("/hello-world", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprint(w, "Hello, world!")
+	})
+	http.ListenAndServe(":8000", nil)
+
 	var a, b string
 	fmt.Println("Введите переменную а:")
-	_, err = fmt.Scanln("%s", &a)
-	if i != nil {
-		fmt.Println(i)
+	_, gut := fmt.Scanln("%s", &a)
+	if a != nil {
+		fmt.Println(a)
 	}
 
 	fmt.Println("Введите переменную b:")
 	fmt.Scanln("%s\n", &b)
-	i = strconv.Atoi(a)
-	z = strconv.Atoi(b)
-
+	i := strconv.Atoi(a)
+	z := strconv.Atoi(b)
+	fmt.Println("Сложение ", a, "+", b)
 }
